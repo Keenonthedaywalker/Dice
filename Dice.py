17c73c438acd8b8d5ad410f21fb2183783b1e475
Keenonthedaywalker <53871946+Keenonthedaywalker@users.noreply.github.com>
2019-11-14 19:41:27 +0200

    Added a Button
    I used kubapilch's PygameUIComponents files.
    
    Link to his file: https://github.com/kubapilch/PygameUI/blob/master/examples/gifs/showcase.gif

--- Dice.py
+++ Dice.py
@@ -1,11 +1,14 @@
 import pygame
 import random
 
+from UIComponents import *
+
 pygame.init()
 
 (WIDTH, HEIGHT) = (800, 600)
 
 WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
@@ -16,26 +19,29 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 randNum = random.randint(1, 6)
 
-font = pygame.font.SysFont('arial', 20)
+def click():
+    if button.color == GREEN:
+        button.color = RED
+    else:
+        button.color = GREEN
 
-text = font.render("Do you want to Roll Again? Y/N", False, RED, WHITE)
 
-gameExit = False
+button_clicked = False
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True        
+button = Button((350, 450, 100, 50), (0, 0), GREEN, 255,  "Re-Roll", font_size=10,  click_function=click)
 
-    SCREEN.fill(BLACK)
-    pygame.draw.rect(SCREEN, WHITE, DICE)
-    
-    
+def butTrue():
+
+    button
+    button_clicked = True
+
+
+def die():
     if randNum == 1:
-            pygame.draw.circle(SCREEN, RED, (400, 300), 10)
+        pygame.draw.circle(SCREEN, RED, (400, 300), 10)
     elif randNum == 2:
-            pygame.draw.circle(SCREEN, RED, (365, 300), 10)
-            pygame.draw.circle(SCREEN, RED, (435, 300), 10)
+        pygame.draw.circle(SCREEN, RED, (365, 300), 10)
+        pygame.draw.circle(SCREEN, RED, (435, 300), 10)
     elif randNum == 3:
         pygame.draw.circle(SCREEN, RED, (330, 230), 10)
         pygame.draw.circle(SCREEN, RED, (395, 290), 10)
@@ -59,8 +65,33 @@ while not gameExit:
         pygame.draw.circle(SCREEN, RED, (393, 260), 10)
         pygame.draw.circle(SCREEN, RED, (393, 350), 10)
 
-    SCREEN.blit(text, (300, 450))
-            
+    
+
+    
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button.clicked(pygame.mouse.get_pos()):
+                print('clicked')
+
+        if button_clicked == True:
+            for i in range(1):
+                die()
+
+    SCREEN.fill(BLACK)
+    pygame.draw.rect(SCREEN, WHITE, DICE)
+    
+  
+    die()
+    butTrue()       
+    button.draw(SCREEN)
     pygame.display.update()
 
 pygame.quit()
